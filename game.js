f2133e3c679db7ff629db8ed9aac7aef35470d03
Ethan McCoy <ethanmccoy@gmail.com>
2024-05-09 01:57:05 -0700

    unspaghettify ui class structure

--- game.js
+++ game.js
@@ -37,28 +37,28 @@ class Enemy {
         this.name = name;
         this.level = level;
         this.type = type;
+
         this.healthBar = scene.add.graphics();
         this.hpText = scene.add.text(0, 0, '', { fontSize: '12px', color: '#ffffff' }).setOrigin(0.5);
-        this.infoText = scene.add.text(
-            this.sprite.x,
-            this.sprite.y + this.sprite.displayHeight / 2 + 30,
-            `${this.name}\nLevel ${this.level} ${this.type}`,
-            { fontSize: '14px', color: '#ffffff', align: 'center', backgroundColor: 'rgba(0, 0, 0, 0.7)' }
-        ).setOrigin(0.5).setInteractive();
+        this.infoText = scene.add.text(x, y + this.sprite.displayHeight / 1.5, `${this.name}\nLevel ${this.level} ${this.type}`, {
+            fontSize: '14px', color: '#ffffff', align: 'center', backgroundColor: 'rgba(0, 0, 0, 0.7)'
+        }).setOrigin(0.5).setInteractive();
 
         this.updateHealthBar();
-        this.updateInfoPosition();
 
         this.sprite.on('pointerdown', this.takeDamage, this);
         this.infoText.on('pointerdown', () => this.scene.showEnemyPopup(this));
     }
 
     takeDamage() {
+        if (this.currentHealth === 0) {
+            return;
+        }
         this.currentHealth = Math.max(0, this.currentHealth - 10);
         this.updateHealthBar();
         if (this.currentHealth === 0) {
             this.sprite.setTexture('enemy_dead');
-            this.scene.showVictoryPopup(this);
+            this.scene.triggerVictory(this);
         }
     }
 
@@ -78,8 +78,6 @@ class Enemy {
 
         this.hpText.setText(`${this.currentHealth} / ${this.maxHealth}`);
         this.hpText.setPosition(this.sprite.x, barY + barHeight / 2);
-
-
     }
 
     updateInfoPosition() {
@@ -94,96 +92,145 @@ class Enemy {
     }
 }
 
-// PopupManager.js
-class PopupManager {
-    constructor(scene) {
+class UIElement {
+    constructor(scene, x, y, width, height) {
         this.scene = scene;
-        this.popups = {};
-
-        // Initialize popups
-        this.createPopup('defaultPopup', 200, 150, () => { });
-        this.createVictoryPopup(() => scene.spawnNewEnemy());
-        this.createPopup('playerInfoPopup', 250, 200, () => { });
+        this.container = scene.add.container(x, y).setSize(width, height);
+    }
+    static fromImage(scene, x, y, imageKey, scale = 1) {
+        const image = scene.add.image(0, 0, imageKey).setScale(scale);
+        const { width, height } = image.getBounds();
+        const uiElement = new UIElement(scene, x, y, width, height);
+        uiElement.container.add(image);
+        return uiElement;
     }
 
-    createPopup(key, width, height, closeCallback) {
-        const container = this.scene.add.container().setVisible(false).setDepth(1);
-        const background = this.createPopupBackground(width, height);
-        const text = this.scene.add.text(20, 20, '', { fontSize: '18px', color: '#ffffff' });
-        const closeButton = this.createCloseButton(() => {
-            closeCallback();
-            this.hidePopup(key);
+    setVisible(visible) {
+        this.container.setVisible(visible);
+    }
+
+}
+
+class Button extends UIElement {
+    constructor(scene, x, y, imageKey, scale = 1, text = '', callback = () => { }) {
+        const uiElement = UIElement.fromImage(scene, x, y, imageKey, scale);
+        const { width, height } = uiElement.container;
+        super(scene, x, y, width, height);
+
+        const image = scene.add.image(0, 0, imageKey).setScale(scale);
+        this.container.add(image);
+
+        if (text) {
+            const textObj = scene.add.text(0, 0, text, {
+                fontSize: '24px',
+                color: '#ffffff',
+                fontStyle: 'bold'
+            });
+            textObj.setOrigin(0.5);
+            this.container.add(textObj);
+        }
+
+        this.container.setSize(width, height)
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', callback);
+    }
+}
+
+
+class PlayerInfo extends UIElement {
+    constructor(scene, x, y, key, scale, playerName, playerLevel) {
+        const element = UIElement.fromImage(scene, 0, 0, key, scale);
+        const { width, height } = element.container.getBounds();
+        super(scene, x, y, width, height);
+
+        // Image
+        this.container.add(element.container);
+
+        // Name
+        this.playerNameText = scene.add.text(0, -height / 5, `Name: ${playerName}`, {
+            fontSize: '16px',
+            fill: '#000000',
+        }).setOrigin(0.5);
+        this.container.add(this.playerNameText);
+
+        // Level
+        this.playerLevelText = scene.add.text(0, height / 5, `Level: ${playerLevel}`, {
+            fontSize: '16px',
+            fill: '#000000',
+        }).setOrigin(0.5);
+        this.container.add(this.playerLevelText);
+
+        // Close button
+        const closeButton = scene.add.text(width / 2 - 50, -height / 2 + 50, 'X', {
+            fontSize: '30px',
+            fill: '#000000',
+            fontStyle: 'bold'
+        }).setOrigin(0.5);
+
+        closeButton.setInteractive({ useHandCursor: true }).on('pointerdown', () => {
+            this.setVisible(false);
         });
 
-        container.add([background, text, closeButton]);
-        this.popups[key] = { container, text };
+        this.container.add(closeButton);
     }
 
-    createPopupBackground(width, height) {
-        const background = this.scene.add.graphics();
-        background.fillStyle(0x000000, 0.8);
-        background.fillRect(0, 0, width, height);
-        return background;
-    }
-
-    createCloseButton(callback) {
-        const closeButton = this.scene.add.text(160, 10, 'X', { fontSize: '24px', color: '#ff0000' });
-        closeButton.setInteractive().on('pointerdown', callback);
-        return closeButton;
-    }
-
-    createVictoryPopup(closeCallback) {
-        this.createPopup('victoryPopup', 200, 150, closeCallback);
-    }
-
-    showPopup(key, textContent, position) {
-        const { x, y, width, height } = position;
-        const { container, text } = this.popups[key];
-        container.setPosition(x - width / 2, y);
-        text.setText(textContent);
-        container.setVisible(true);
-    }
-
-    hidePopup(key) {
-        this.popups[key].container.setVisible(false);
-    }
-
-    showDefaultPopup(textContent, position) {
-        this.showPopup('defaultPopup', textContent, position);
-    }
-
-    showVictoryPopup(textContent, position) {
-        this.showPopup('victoryPopup', textContent, position);
-    }
-
-    showPlayerInfoPopup(textContent, position) {
-        this.showPopup('playerInfoPopup', textContent, position);
+    updatePlayerInfo(playerName, playerLevel) {
+        this.playerNameText.setText(`Name: ${playerName}`);
+        this.playerLevelText.setText(`Level: ${playerLevel}`);
     }
 }
 
-// PlayerInfoButton.js
-class PlayerInfoButton {
-    constructor(scene, callback) {
-        const buttonWidth = 200;
-        const buttonHeight = 50;
-        const buttonX = (scene.scale.width / 2) - (buttonWidth / 2);
-        const buttonY = scene.scale.height - buttonHeight - 20;
 
-        const background = scene.add.graphics();
-        background.fillStyle(0x000000, 1);
-        background.fillRect(buttonX, buttonY, buttonWidth, buttonHeight);
+class VictoryInfo extends UIElement {
+    constructor(scene, x, y, key, scale, xp, gold) {
+        const element = UIElement.fromImage(scene, 0, 0, key, scale);
+        const { width, height } = element.container.getBounds();
+        super(scene, x, y, width, height);
 
-        const buttonText = scene.add.text(buttonX + 10, buttonY + 10, 'Player Info', {
-            fontSize: '24px',
-            color: '#ffffff'
-        }).setOrigin(0);
+        // Image
+        this.container.add(element.container);
 
-        buttonText.setInteractive({ useHandCursor: true })
-            .on('pointerdown', callback);
+        this.victoryText = scene.add.text(0, -150, `Victory!`, {
+            fontSize: '32px',
+            fill: '#000000',
+        }).setOrigin(0.5);
+        this.container.add(this.victoryText);
 
-        this.buttonContainer = scene.add.container().add([background, buttonText]);
+        // Xp
+        this.xpText = scene.add.text(0, -20, `XP: ${xp}`, {
+            fontSize: '16px',
+            fill: '#000000',
+        }).setOrigin(0.5);
+        this.container.add(this.xpText);
+
+        // Gold
+        this.goldText = scene.add.text(0, 20, `Gold: ${gold}`, {
+            fontSize: '16px',
+            fill: '#000000',
+        }).setOrigin(0.5);
+        this.container.add(this.goldText);
+
+        // Close button
+        const closeButton = scene.add.text(0, 150, 'Continue', {
+            fontSize: '30px',
+            fill: '#000000',
+            fontStyle: 'bold'
+        }).setOrigin(0.5);
+
+        closeButton.setInteractive({ useHandCursor: true }).on('pointerdown', () => {
+            this.setVisible(false);
+            scene.spawnNewEnemy();
+        });
+
+        this.container.add(closeButton);
+    }
+
+    updatePlayerInfo(playerName, playerLevel) {
+        this.playerNameText.setText(`Name: ${playerName}`);
+        this.playerLevelText.setText(`Level: ${playerLevel}`);
     }
 }
+
 
 
 class GameScene extends Phaser.Scene {
@@ -191,10 +238,10 @@ class GameScene extends Phaser.Scene {
         super({ key: 'GameScene' });
         this.currentEnemy = null;
         this.enemyList = [
-            { key: 'enemy', name: 'Blazing Cat', level: 45, type: 'Fire Elemental', maxHealth: 100 },
-            { key: 'enemy', name: 'Frost Cat', level: 35, type: 'Ice Elemental', maxHealth: 80 },
-            { key: 'enemy', name: 'Storm Cat', level: 50, type: 'Electric Elemental', maxHealth: 120 },
-            { key: 'enemy', name: 'Stone Cat', level: 60, type: 'Earth Elemental', maxHealth: 150 }
+            { key: 'enemy', name: 'Blazing Cat', level: 45, type: 'Fire Elemental', maxHealth: 90 },
+            { key: 'enemy', name: 'Frost Cat', level: 35, type: 'Ice Elemental', maxHealth: 110 },
+            { key: 'enemy', name: 'Storm Cat', level: 50, type: 'Electric Elemental', maxHealth: 80 },
+            { key: 'enemy', name: 'Stone Cat', level: 60, type: 'Earth Elemental', maxHealth: 120 }
         ];
     }
 
@@ -203,7 +250,9 @@ class GameScene extends Phaser.Scene {
             { key: 'hero', path: 'dog.jpg' },
             { key: 'enemy', path: 'cat.jpg' },
             { key: 'enemy_dead', path: 'cat_dead.png' },
-            { key: 'background', path: 'bg_grass.jpg' }
+            { key: 'background', path: 'bg_grass.jpg' },
+            { key: 'button', path: 'button.png' },
+            { key: 'scroll', path: 'scroll.png' }
         ]);
     }
 
@@ -213,45 +262,35 @@ class GameScene extends Phaser.Scene {
 
     create() {
         this.add.image(this.scale.width, this.scale.height / 3, 'background').setScale(0.5).setScrollFactor(0);
-        this.popupManager = new PopupManager(this);
-        this.playerInfoButton = new PlayerInfoButton(this, this.showPlayerInfoPopup.bind(this));
-        this.player = new Player(this);
 
-        this.spawnNewEnemy();
-    }
+        const playerInfo = new PlayerInfo(this, 200, 650, 'scroll', 0.5, 'Ethan', 1);
+        playerInfo.setVisible(false);
+        this.add.existing(playerInfo.container);
 
-    showPlayerInfoPopup() {
-        const textContent = this.player.getInfo();
-        const popupWidth = 250;
-        const popupHeight = 200;
-        const popupX = this.scale.width / 2;
-        const popupY = this.scale.height - popupHeight - 20;
-
-        this.popupManager.showPlayerInfoPopup(textContent, {
-            x: popupX,
-            y: popupY,
-            width: popupWidth,
-            height: popupHeight
+        const playerInfoButton = new Button(this, 200, 825, 'button', 0.25, 'Player Info', () => {
+            playerInfo.setVisible(true);
         });
+        this.add.existing(playerInfoButton.container);
+
+
+
+        this.player = new Player(this);
+        this.spawnNewEnemy();
     }
 
     showEnemyPopup(enemy) {
         const textContent = `Name: ${enemy.name}\nLevel: ${enemy.level}\nType: ${enemy.type}\nHealth: ${enemy.currentHealth}/${enemy.maxHealth}`;
-        this.popupManager.showDefaultPopup(textContent, {
-            x: enemy.sprite.x,
-            y: enemy.sprite.y,
-            width: 200,
-            height: 150
-        });
+        this.enemyInfo.setText(textContent);
+        this.enemyInfo.setVisible(true);
     }
-    showVictoryPopup(enemy) {
-        const textContent = `Victory!\nExp: 100\nGold: 15`;
-        this.popupManager.showVictoryPopup(textContent, {
-            x: enemy.sprite.x,
-            y: enemy.sprite.y,
-            width: 200,
-            height: 150
-        });
+
+    triggerVictory(enemy) {
+        this.player.addXp(enemy.level * 10);
+        this.player.addGold(enemy.level * 5);
+
+        const victoryInfo = new VictoryInfo(this, 200, 500, 'scroll', 0.5, enemy.level * 10, enemy.level * 5);
+        this.add.existing(victoryInfo.container);
+        victoryInfo.setVisible(true);
     }
 
     spawnNewEnemy() {
@@ -269,9 +308,7 @@ class GameScene extends Phaser.Scene {
     }
 
     update() {
-        if (this.currentEnemy) {
-            this.currentEnemy.updateInfoPosition();
-        }
+
     }
 }
 
